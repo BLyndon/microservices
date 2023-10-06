13c15f4e3222ab4d20e4dc3f184a2644d672e324
BLyndon <bheil@mail.de>
2023-10-06 23:56:27 +0200

    add Fraud microservice

---
+++ customer/src/main/java/com/blyndon/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.blyndon;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ fraud/src/main/java/com/blyndon/FraudCheckHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.blyndon;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistory, Integer> {
+
+}
