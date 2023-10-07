ebdec00b5bc6dd609e04ea24bda13697930fa8bc
BLyndon <bheil@mail.de>
2023-10-07 11:20:25 +0200

    remove hardcoded port

--- customer/src/main/java/com/blyndon/customer/CustomerService.java
+++ customer/src/main/java/com/blyndon/customer/CustomerService.java
@@ -19,12 +19,12 @@ public class CustomerService {
                 .build();
         customerRepository.saveAndFlush(customer);
         final FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
 
-        if(fraudCheckResponse.isFraudster()) {
+        if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster ");
         }
     }
