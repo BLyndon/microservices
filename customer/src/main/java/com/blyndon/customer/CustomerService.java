3ee3bb7be33a1d9b1e64681ecb5cd91e603b8a62
BLyndon <bheil@mail.de>
2023-10-07 11:51:38 +0200

    replace rest template by feign client

--- customer/src/main/java/com/blyndon/customer/CustomerService.java
+++ customer/src/main/java/com/blyndon/customer/CustomerService.java
@@ -1,15 +1,16 @@
 package com.blyndon.customer;
 
+import com.blyndon.clients.fraud.FraudCheckResponse;
+import com.blyndon.clients.fraud.FraudClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
 
-    private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
+    private CustomerRepository customerRepository;
+    private FraudClient fraudClient;
 
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
@@ -18,11 +19,8 @@ public class CustomerService {
                 .email(customerRequest.email())
                 .build();
         customerRepository.saveAndFlush(customer);
-        final FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-        );
+
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
 
         if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster ");
