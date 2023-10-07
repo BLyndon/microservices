64c03816fcffbf24c11ff38a2eead91d051e35da
BLyndon <bheil@mail.de>
2023-10-07 18:15:19 +0200

    add notification microservice

--- customer/src/main/java/com/blyndon/customer/CustomerService.java
+++ customer/src/main/java/com/blyndon/customer/CustomerService.java
@@ -2,6 +2,8 @@ package com.blyndon.customer;
 
 import com.blyndon.clients.fraud.FraudCheckResponse;
 import com.blyndon.clients.fraud.FraudClient;
+import com.blyndon.clients.notification.NotificationClient;
+import com.blyndon.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,21 +11,33 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class CustomerService {
 
-    private CustomerRepository customerRepository;
-    private FraudClient fraudClient;
+    private final CustomerRepository customerRepository;
+    private final NotificationClient notificationClient;
+    private final FraudClient fraudClient;
 
-    public void registerCustomer(CustomerRegistrationRequest customerRequest) {
+    public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
-                .firstName(customerRequest.firstName())
-                .lastName(customerRequest.lastName())
-                .email(customerRequest.email())
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
                 .build();
         customerRepository.saveAndFlush(customer);
 
-        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
+        FraudCheckResponse fraudCheckResponse =
+                fraudClient.isFraudster(customer.getId());
 
         if (fraudCheckResponse.isFraudster()) {
-            throw new IllegalStateException("fraudster ");
+            throw new IllegalStateException("fraudster");
         }
+
+        notificationClient.sendNotification(
+                new NotificationRequest(
+                        customer.getId(),
+                        customer.getEmail(),
+                        String.format("Hi %s, welcome to Blyndon Bank......",
+                                customer.getFirstName())
+                )
+        );
+
     }
 }
--- customer/src/main/java/com/blyndon/customer/FraudCheckResponse.java
+++
@@ -1,4 +0,0 @@
-package com.blyndon.customer;
-
-public record FraudCheckResponse(Boolean isFraudster) {
-}
